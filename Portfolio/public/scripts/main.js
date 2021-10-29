b9a9355aa3ca7d0b79301907a351a2383ec915f9
scottjm1 <jasminescott2018@gmail.com>
2021-10-29 10:05:26 -0400

    Animations for about page

--- Portfolio/public/scripts/main.js
+++ Portfolio/public/scripts/main.js
@@ -14,18 +14,55 @@ rhit.variableName = "Jasmine Scott";
 
 /** function and class syntax examples */
 
-rhit.transitionEffect = function (element, className) {
-	let aboutImage = document.querySelector(element);
-	aboutImage.classList.remove("hidden");
-	aboutImage.classList.add(className);
+rhit.AboutController = class {
+	constructor() {
+
+		transitionEffect("#aboutImg", "resizing-img", false, 3000);
+		transitionEffect("#aboutParagraphs", "resizing-text", false, 6000);
+		transitionEffect("footer", "fade-in-footer", false, 9000);
+
+
+		document.querySelector("#portfolioLink").addEventListener("click", (event) => {
+			console.log("CLICK");
+			transitionEffect("footer", "fade-in-footer-reverse", true, 0);
+			transitionEffect("#aboutParagraphs", "resizing-text-reverse", true, 500);
+			transitionEffect("#aboutImg", "resizing-img-reverse", true, 1000);
+			transitionEffect("#aboutHeader", "resizing-header-reverse", true, 1500);
+			redirect();
+		});
+
+	}
+}
+
+const transitionEffect = async (element, className, reverse, mil) => {
+	let ele = document.querySelector(element);
+
+	if (!reverse) {
+		await delay(mil);
+		ele.classList.remove("hidden");
+		ele.classList.add(className);
+	} else {
+		await delay(mil);
+		ele.classList.add(className);
+		await delay(1000);
+		ele.classList.add("hidden");
+	}
 };
 
-async function transitions(element, mil, className) {
+const redirect = async () => {
+	await delay(3000);
+	window.location.href = "portfolio.html";
+}
+
+// https://stackoverflow.com/questions/14226803/wait-5-seconds-before-executing-next-line
+async function transitions(element, mil, className, reverse) {
 	return await Promise.resolve(setTimeout(function () {
-		rhit.transitionEffect(element, className);
+		rhit.transitionEffect(element, className, reverse);
 	}, mil));
 };
 
+
+const delay = ms => new Promise(res => setTimeout(res, ms));
 
 
 /* Main */
@@ -34,8 +71,7 @@ rhit.main = function () {
 	console.log("Ready");
 
 	if (document.querySelector("#aboutPage")) {
-		transitions("#aboutImg", 3000, "resizing-img").then(transitions("#aboutParagraphs", 6000, "resizing-text"));
-		transitions("footer", 9000, "fade-in-footer");
+		new rhit.AboutController();
 	}
 
 
